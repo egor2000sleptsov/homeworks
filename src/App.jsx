7e1fb34508d848e720be654e582438780d563310
Egor <egor2000sleptsov@gmail.com>
2023-04-07 02:31:21 +1000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import User from "./components/User.jsx";
 
 function App() {
+		const usernames = ['egor2000sleptsov', 'Crack-Man']
+		const [users, setUsers] = useState([])
 
-  return (
-    <div className="App">
-    </div>
-  )
+		useEffect(() => {
+				const getUsersFromGithub = async usernames => {
+						const res = []
+						for (const username of usernames) {
+								let user = await fetch(`https://api.github.com/users/${username}`)
+								user = await user.json()
+								res.push(user)
+						}
+						setUsers(res)
+				}
+				getUsersFromGithub(usernames)
+		}, [])
+
+		return (
+				<div className="App">
+						{users.map((el, index) => <User key={index} user={el}/>)}
+				</div>
+		)
 }
 
 export default App
